41d441670afb2858f998cbf1957c34eaa2a48d7d
rohanrai123 <108138780+rohanrai123@users.noreply.github.com>
2024-08-24 06:09:12 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("ebba8f3b53ad758b7f72eca9bf3ff7c52171abfbe6cd7fc12ff5ef86ccc69a5d","mikasa"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
